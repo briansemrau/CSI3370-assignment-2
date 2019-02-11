3d48aa805c6bd60ff30a63b99ec6d3c2479a033c
Brian Semrau <brian.semrau@gmail.com>
2019-02-11 16:31:39 -0500

    Implemented ninetyNineBottlesOfBeer to spec

--- BeerSong.java
+++ BeerSong.java
@@ -6,7 +6,12 @@ public class BeerSong {
 	}
 	
 	private static void ninetyNineBottlesOfBeer() {
-		// To do
+		int bottles = 99;
+		while (bottles > 2) {
+			System.out.println("" + bottles + " bottles of beer on the wall, " + bottles + " bottles of beer\ntake one down, pass it around, " + --bottles + " bottles of beer on the wall.");
+		}
+		// Last two lines of the song (unique wording)
+		System.out.println("2 bottles of beer on the wall, 2 bottles of beer\ntake one down, pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer\ntake one down, pass it around, no more bottles of beer on the wall.");
 	}
 	
 }
